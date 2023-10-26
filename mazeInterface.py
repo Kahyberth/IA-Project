0cf6b4e9b748b0be2664b8704f55d76ab8b4c2fd
Setchko <Juankvarela@outlook.es>
2023-10-26 02:30:55 -0500

    Interface
    add detail

--- mazeInterface.py
+++ mazeInterface.py
@@ -1,3 +1,5 @@
+import time
+
 import pygame
 import sys
 from maze import Maze
@@ -30,6 +32,9 @@ matriz_prueba = [
     [0, 3, 0, 4, 0, 0, 0, 0]
 ]
 
+# Definir la posición inicial del agente
+posicion_agente = [2, 7]
+
 # Crear una instancia de la clase Maze
 #maze = Maze()
 matriz = matriz_prueba  # Obtener la matriz del laberinto
@@ -48,8 +53,11 @@ def dibujar_tablero(matriz):
             pygame.draw.rect(screen, color_borde, (x, y, cell_width, cell_width), grosor_borde)
 
             characters = matriz[fila][columna]
-            if characters == 1: #Agente
-                screen.blit(agente_image, (x, y))
+            if characters == 1:  # Agente
+                x_agente = x + cell_width // 2  # Posición X del centro de la celda
+                y_agente = y + cell_width // 2  # Posición Y del centro de la celda
+                screen.blit(agente_image,
+                             (x_agente - agente_image.get_width() // 2, y_agente - agente_image.get_height() // 2))
             if characters == 4: #Katz
                 screen.blit(katz_image, (x, y))
             if characters == 5: #Muriel
@@ -59,6 +67,8 @@ def dibujar_tablero(matriz):
             if characters == 2: #Obstaculo
                 screen.blit(obstacle_image, (x,y))
 
+# Camino prueba
+pathfinder = [(2, 7), (3, 7), (4, 7), (4, 6), (4, 5), (4, 4), (4, 3), (4, 2), (4, 1), (4, 0), (3, 0), (2, 0)]
 
 # Bucle principal (Mantiene la pantalla hasta que se cierre)
 while True:
@@ -66,6 +76,25 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    if pathfinder:
+        nueva_posicion = pathfinder.pop(0)
+        x1, y1 = posicion_agente
+        x2, y2 = nueva_posicion
+
+        # Animación del movimiento del agente
+        for i in range(10):  # Realiza 10 pasos para la animación
+            inter_x = x1 + (x2 - x1) * i / 10
+            inter_y = y1 + (y2 - y1) * i / 10
+            matriz[x1][y1] = 0
+            matriz[int(inter_x)][int(inter_y)] = 1
+            dibujar_tablero(matriz)
+            pygame.display.update()
+            time.sleep(0.1)
+
+        matriz[x1][y1] = 0
+        matriz[x2][y2] = 1
+        posicion_agente = nueva_posicion
 
     # Dibujar el tablero
     dibujar_tablero(matriz)
